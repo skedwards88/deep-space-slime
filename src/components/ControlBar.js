d59b3c6d9d7d24121ea3c80ee095e5d1dc5565f8
skedwards88 <skedwards88@github.com>
2024-09-26 08:16:38 -0700

    add share button to control bar

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -1,14 +1,32 @@
 import React from "react";
+import {handleShare} from "../common/handleShare";
 import packageJson from "../../package.json";
 
 function ControlBar({setDisplay}) {
   return (
     <div id="controls">
       <button
-        id="mapButton"
+        id="mapIcon"
         className="controlButton"
         onClick={() => setDisplay("map")}
       ></button>
+
+      {navigator.canShare ? (
+        <button
+          id="shareIcon"
+          className="controlButton"
+          onClick={() =>
+            handleShare({
+              appName: "Crossjig",
+              text: "Check out this word game!",
+              url: "https://crossjig.com",
+            })
+          }
+        ></button>
+      ) : (
+        <></>
+      )}
+
       {/* todo remove version number once done testing */}
       <small id="rulesVersion">version {packageJson.version}</small>
     </div>
