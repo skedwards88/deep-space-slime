ad8b401a6100f1107d866d0dccb1263ffeac39a5
skedwards88 <skedwards88@github.com>
2024-09-26 08:02:59 -0700

    add note

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -9,6 +9,7 @@ function ControlBar({setDisplay}) {
         className="controlButton"
         onClick={() => setDisplay("map")}
       ></button>
+      {/* todo remove version number once done testing */}
       <small id="rulesVersion">version {packageJson.version}</small>
     </div>
   );
