a5f43e9d78175c32dcf72eb16362642fd9e799ed
skedwards88 <skedwards88@github.com>
2024-09-28 08:55:48 -0700

    use saved state

--- src/logic/gameInit.js
+++ src/logic/gameInit.js
@@ -2,23 +2,70 @@ import sendAnalytics from "../common/sendAnalytics";
 import {getValidNextIndexes} from "./getValidNextIndexes";
 import {puzzles} from "./puzzles";
 
+function validateSavedState(savedState) {
+  // saved state must exist
+  if (typeof savedState !== "object" || savedState === null) {
+    return false;
+  }
+
+  // puzzleID must be an int between 0 and number of puzzles
+  if (!Number.isInteger(savedState.puzzleID)) {
+    return false;
+  }
+  if (savedState.puzzleID < 0) {
+    return false;
+  }
+  if (savedState.puzzleID > puzzles.length - 1) {
+    return false;
+  }
+
+  // mainPath must be array of ints
+  if (!Array.isArray(savedState.mainPath)) {
+    return false;
+  }
+  if (savedState.mainPath.some((entry) => !Number.isInteger(entry))) {
+    return false;
+  }
+
+  // acquired features must be ints
+  if (
+    !Number.isInteger(savedState.flaskCount) ||
+    !Number.isInteger(savedState.keyCount) ||
+    !Number.isInteger(savedState.jetCount) ||
+    !Number.isInteger(savedState.numberCount) ||
+    !Number.isInteger(savedState.maxNumber)
+  ) {
+    return false;
+  }
+
+  // dimensions are as expected
+  if (savedState.numColumns !== 7 || savedState.numRows !== 9) {
+    return false;
+  }
+
+  // validNextIndexes must be array of ints
+  if (!Array.isArray(savedState.validNextIndexes)) {
+    return false;
+  }
+  if (savedState.validNextIndexes.some((entry) => !Number.isInteger(entry))) {
+    return false;
+  }
+
+  return true;
+}
+
 export function gameInit({useSaved = true, puzzleID = 0}) {
   const savedState = useSaved
     ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))
     : undefined;
 
-  if (
-    savedState
-    // todo enter other requirements for using saved state here
-  ) {
-    // return savedState;
+  if (savedState && validateSavedState(savedState)) {
+    return savedState;
   }
 
   sendAnalytics("new_game");
 
   const puzzle = puzzles[puzzleID].puzzle;
-
-  // todo If a square has been visited, any icon on that space becomes transparent.
 
   const numColumns = 7;
   const numRows = 9;
@@ -48,6 +95,5 @@ export function gameInit({useSaved = true, puzzleID = 0}) {
     maxNumber,
     validNextIndexes,
     message: puzzles[puzzleID].startingText,
-    score: 0,
   };
 }
