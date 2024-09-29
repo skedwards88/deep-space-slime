6f625b764bfde386188704e9ae77a1c7d3042226
skedwards88 <skedwards88@github.com>
2024-09-29 14:24:03 -0700

    remove unused param

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {handleShare} from "../common/handleShare";
 
-export default function Share({appName, text, seed, url}) {
+export default function Share({appName, text, url}) {
   if (navigator.canShare) {
     return (
       <button onClick={() => handleShare({appName, text, url})}>Share</button>
