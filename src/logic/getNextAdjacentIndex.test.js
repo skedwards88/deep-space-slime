27433ec2f8f0e8ac38bcb373fff58c6ca59a94da
skedwards88 <skedwards88@github.com>
2024-09-29 11:50:11 -0700

    add tests for uneven grid

--- src/logic/getNextAdjacentIndex.test.js
+++ src/logic/getNextAdjacentIndex.test.js
@@ -1,6 +1,6 @@
 import {getNextAdjacentIndex} from "./getNextAdjacentIndex";
 
-describe("getNextAdjacentIndex", () => {
+describe("getNextAdjacentIndex (3x3 grid)", () => {
   test("returns next adjacent index for a left-right horizontal line", () => {
     const result = getNextAdjacentIndex({
       index: 0,
@@ -170,5 +170,167 @@ describe("getNextAdjacentIndex", () => {
         numRows: 3,
       }),
     ).toThrow("Input cells are not adjacent.");
+  });
+});
+
+describe("getNextAdjacentIndex (7x9 grid)", () => {
+  test("returns next adjacent index for a left-right horizontal line", () => {
+    const result = getNextAdjacentIndex({
+      index: 0,
+      adjacentIndex: 1,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(2);
+  });
+
+  test("returns next adjacent index for a right-left horizontal line", () => {
+    const result = getNextAdjacentIndex({
+      index: 2,
+      adjacentIndex: 1,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(0);
+  });
+
+  test("returns next adjacent index for a up-down vertical line", () => {
+    const result = getNextAdjacentIndex({
+      index: 0,
+      adjacentIndex: 7,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(14);
+  });
+
+  test("returns next adjacent index for a down-up vertical line", () => {
+    const result = getNextAdjacentIndex({
+      index: 14,
+      adjacentIndex: 7,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(0);
+  });
+
+  test("returns next adjacent index for a diagonal line (top-left to bottom-right)", () => {
+    const result = getNextAdjacentIndex({
+      index: 0,
+      adjacentIndex: 8,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(16);
+  });
+
+  test("returns next adjacent index for a diagonal line (bottom-right to top-left)", () => {
+    const result = getNextAdjacentIndex({
+      index: 62,
+      adjacentIndex: 54,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(46);
+  });
+
+  test("returns next adjacent index for a diagonal line (bottom-left to top-right)", () => {
+    const result = getNextAdjacentIndex({
+      index: 56,
+      adjacentIndex: 50,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(44);
+  });
+
+  test("returns next adjacent index for a diagonal line (top-right to bottom-left)", () => {
+    const result = getNextAdjacentIndex({
+      index: 6,
+      adjacentIndex: 12,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBe(18);
+  });
+
+  test("returns undefined if no adjacent cell (left-right)", () => {
+    const result = getNextAdjacentIndex({
+      index: 5,
+      adjacentIndex: 6,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test("returns undefined if no adjacent cell (right-left)", () => {
+    const result = getNextAdjacentIndex({
+      index: 8,
+      adjacentIndex: 7,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test("returns undefined if no adjacent cell (left-right, off grid)", () => {
+    const result = getNextAdjacentIndex({
+      index: 1,
+      adjacentIndex: 0,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test("returns undefined if no adjacent cell (right-left, off grid)", () => {
+    const result = getNextAdjacentIndex({
+      index: 61,
+      adjacentIndex: 62,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test("returns undefined if no adjacent cell (top-bottom)", () => {
+    const result = getNextAdjacentIndex({
+      index: 49,
+      adjacentIndex: 56,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test("returns undefined if no adjacent cell (bottom-top)", () => {
+    const result = getNextAdjacentIndex({
+      index: 9,
+      adjacentIndex: 2,
+      numColumns: 7,
+      numRows: 9,
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test("errors if either input index is outside of grid", () => {
+    expect(() =>
+      getNextAdjacentIndex({
+        index: 63,
+        adjacentIndex: 62,
+        numColumns: 7,
+        numRows: 9,
+      }),
+    ).toThrow("Input index 63 exceeds the array size 63");
+
+    expect(() =>
+      getNextAdjacentIndex({
+        index: 62,
+        adjacentIndex: 63,
+        numColumns: 7,
+        numRows: 9,
+      }),
+    ).toThrow("Input index 63 exceeds the array size 63");
   });
 });
