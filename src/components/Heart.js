b6fe89e9973b0f6339a449aa073fde3999603f07
skedwards88 <skedwards88@github.com>
2024-10-01 15:22:38 -0700

    link to patreon

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -9,6 +9,8 @@ export default function Heart({setDisplay, appName, shareText, repoName, url}) {
         <p>Like this game? Share it with your friends.</p>
         {<Share appName={appName} text={shareText} url={url}></Share>}
         <hr></hr>
+        <p>Follow our <a href="https://www.patreon.com/skedwards88"><strong>FREE</strong> Patreon</a> to learn about new releases.</p>
+        <hr></hr>
         <p>
           Want more games? See all of our puzzle games{" "}
           <a href="https://skedwards88.github.io/">here</a> .
