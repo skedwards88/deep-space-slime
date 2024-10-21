8af7506c2e7e2dcb1514c5134597afa7ce9c182f
Sarah Edwards <skedwards88@gmail.com>
2024-10-21 07:17:25 -0700

    Revert "Revert "Custom builder""

--- src/logic/gameInit.js
+++ src/logic/gameInit.js
@@ -1,29 +1,68 @@
 import sendAnalytics from "../common/sendAnalytics";
-import {convertPuzzleToString} from "./convertPuzzleString";
 import {getValidNextIndexes} from "./getValidNextIndexes";
 import {puzzles} from "./puzzles";
 import {validateSavedState} from "./validateSavedState";
+import {validateBuilder} from "./validateBuilder";
+import {
+  convertStringToPuzzle,
+  convertPuzzleToString,
+} from "./convertPuzzleString";
 
-export function gameInit({useSaved = true, puzzleID = 0}) {
+export function gameInit({
+  useSaved = true,
+  puzzleID = 0,
+  isCustom = false,
+  customSeed,
+  customIndex,
+}) {
+  const numColumns = 7;
+  const numRows = 9;
+
+  // If custom, convert the query string into a puzzle
+  let customName;
+  let customEncodedPuzzle;
+  let customPuzzle;
+  if (isCustom) {
+    try {
+      [customName, customEncodedPuzzle] = customSeed.split("_");
+      customName = customName.replaceAll("+", " ");
+      customPuzzle = convertStringToPuzzle(customEncodedPuzzle);
+
+      // Mane sure that the puzzle passes all of the validation (in case someone edits/mangles the query string)
+      const {isValid} = validateBuilder({
+        puzzle: customPuzzle,
+        numColumns,
+        numRows,
+      });
+      if (!isValid) {
+        throw new Error("Custom puzzle is not valid.");
+      }
+    } catch {
+      console.log("Error generating custom puzzle from query.");
+      isCustom = false;
+    }
+  }
+
   const savedState = useSaved
     ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))
     : undefined;
 
-  if (savedState && validateSavedState(savedState)) {
+  if (
+    savedState &&
+    (!isCustom || (isCustom && customSeed === savedState.encodedPuzzle)) &&
+    validateSavedState(savedState)
+  ) {
     return {...savedState, mouseIsActive: false};
   }
 
   sendAnalytics("new_game", {puzzleID});
 
-  const puzzle = puzzles[puzzleID].puzzle;
+  const puzzle = isCustom ? customPuzzle : puzzles[puzzleID].puzzle;
 
   // Get a string representation of the puzzle so that
   // we can later verify that the puzzle has not been updated
   // since the player started solving it
   const encodedPuzzle = convertPuzzleToString(puzzle);
-
-  const numColumns = 7;
-  const numRows = 9;
 
   const startIndex = puzzle.indexOf("start");
   const mainPath = [startIndex];
@@ -39,9 +78,24 @@ export function gameInit({useSaved = true, puzzleID = 0}) {
     maxNumber,
   });
 
+  const defaultText = isCustom
+    ? "This is a custom puzzle built by a human subject."
+    : puzzles[puzzleID].startingText;
+
   return {
-    puzzleID,
-    mainPath,
+    isCustom,
+    customIndex,
+    station: isCustom ? "Custom Simulation" : puzzles[puzzleID].station,
+    room: isCustom ? customName : puzzles[puzzleID].room,
+    startingText: defaultText,
+    hintText: isCustom ? undefined : puzzles[puzzleID].hintText,
+    winText: isCustom
+      ? "You solved the custom puzzle! You can edit or share the custom puzzle, or return to the main game."
+      : puzzles[puzzleID].winText,
+    message: defaultText,
+    robotStartMood: isCustom ? "happy" : puzzles[puzzleID].robotStartMood,
+    robotEndMood: isCustom ? "happy" : puzzles[puzzleID].robotEndMood,
+    puzzle,
     numColumns,
     numRows,
     flaskCount: 0,
@@ -50,8 +104,9 @@ export function gameInit({useSaved = true, puzzleID = 0}) {
     numberCount: 0,
     maxNumber,
     validNextIndexes,
-    message: puzzles[puzzleID].startingText,
+    mainPath,
     encodedPuzzle,
     mouseIsActive: false,
+    puzzleID: isCustom ? "custom" : puzzleID,
   };
 }
