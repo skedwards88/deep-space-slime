93613cb7f012805762aeac6fb0d53e1839fe612b
Sarah <skedwards88@gmail.com>
2024-10-23 18:52:23 -0700

    better validation for custom init games

--- src/logic/gameInit.js
+++ src/logic/gameInit.js
@@ -1,12 +1,9 @@
 import sendAnalytics from "../common/sendAnalytics";
 import {getValidNextIndexes} from "./getValidNextIndexes";
 import {puzzles} from "./puzzles";
-import {validateSavedState} from "./validateSavedState";
+import {validateSavedState, puzzleIdIsValid} from "./validateSavedState";
 import {validateBuilder} from "./validateBuilder";
-import {
-  convertStringToPuzzle,
-  convertPuzzleToString,
-} from "./convertPuzzleString";
+import {convertStringToPuzzle} from "./convertPuzzleString";
 
 export function gameInit({
   useSaved = true,
@@ -17,6 +14,62 @@ export function gameInit({
 }) {
   const numColumns = 7;
   const numRows = 9;
+  const customStationName = "Custom Simulation"; // todo could set elsewhere for import
+  const customWinText =
+    "You solved the custom puzzle! You can edit or share the custom puzzle, or return to the main game.";
+  const customStartingText =
+    "This is a custom puzzle built by a human subject.";
+  const customHintText = undefined;
+  const customRobotMood = "happy";
+
+  // Return the saved state if we can
+  const savedState = useSaved
+    ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))
+    : undefined;
+
+  if (savedState && validateSavedState(savedState)) {
+    return {
+      ...savedState,
+      mouseIsActive: false,
+      // Overwrite these properties in case we changed them mid-play.
+      // They don't affect the puzzle, so we don't need to reset the player's progress.
+      station: savedState.isCustom
+        ? customStationName
+        : puzzles[savedState.puzzleID].station,
+      room: savedState.isCustom
+        ? customName
+        : puzzles[savedState.puzzleID].room,
+      startingText: savedState.isCustom
+        ? customStartingText
+        : puzzles[savedState.puzzleID].startingText,
+      hintText: savedState.isCustom
+        ? customHintText
+        : puzzles[savedState.puzzleID].hintText,
+      winText: savedState.isCustom
+        ? customWinText
+        : puzzles[savedState.puzzleID].winText,
+      message: savedState.isCustom
+        ? customStartingText
+        : puzzles[savedState.puzzleID].startingText,
+      robotStartMood: savedState.isCustom
+        ? customRobotMood
+        : puzzles[savedState.puzzleID].robotStartMood,
+      robotEndMood: savedState.isCustom
+        ? customRobotMood
+        : puzzles[savedState.puzzleID].robotEndMood,
+    };
+  }
+
+  // If the saved state wasn't valid but we were instructed to use the saved state,
+  // use the puzzleID from the saved state if possible
+  if (
+    useSaved &&
+    savedState &&
+    !savedState.isCustom &&
+    puzzleIdIsValid(savedState.puzzleID)
+  ) {
+    puzzleID = savedState.puzzleID;
+  }
 
   // If custom, convert the query string into a puzzle
   let customName;
@@ -37,32 +90,13 @@ export function gameInit({
       if (!isValid) {
         throw new Error("Custom puzzle is not valid.");
       }
-    } catch {
+    } catch (error) {
       console.log("Error generating custom puzzle from query.");
       isCustom = false;
     }
   }
 
-  const savedState = useSaved
-    ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))
-    : undefined;
-
-  if (
-    savedState &&
-    (!isCustom || (isCustom && customSeed === savedState.encodedPuzzle)) &&
-    validateSavedState(savedState)
-  ) {
-    return {...savedState, mouseIsActive: false};
-  }
-
-  sendAnalytics("new_game", {puzzleID});
-
   const puzzle = isCustom ? customPuzzle : puzzles[puzzleID].puzzle;
-
-  // Get a string representation of the puzzle so that
-  // we can later verify that the puzzle has not been updated
-  // since the player started solving it
-  const encodedPuzzle = convertPuzzleToString(puzzle);
 
   const startIndex = puzzle.indexOf("start");
   const mainPath = [startIndex];
@@ -78,23 +112,24 @@ export function gameInit({
     maxNumber,
   });
 
-  const defaultText = isCustom
-    ? "This is a custom puzzle built by a human subject."
-    : puzzles[puzzleID].startingText;
+  sendAnalytics("new_game", {puzzleID: isCustom ? "custom" : puzzleID});
 
   return {
     isCustom,
     customIndex,
-    station: isCustom ? "Custom Simulation" : puzzles[puzzleID].station,
+    puzzleID: isCustom ? "custom" : puzzleID,
+    station: isCustom ? customStationName : puzzles[puzzleID].station,
     room: isCustom ? customName : puzzles[puzzleID].room,
-    startingText: defaultText,
-    hintText: isCustom ? undefined : puzzles[puzzleID].hintText,
-    winText: isCustom
-      ? "You solved the custom puzzle! You can edit or share the custom puzzle, or return to the main game."
-      : puzzles[puzzleID].winText,
-    message: defaultText,
-    robotStartMood: isCustom ? "happy" : puzzles[puzzleID].robotStartMood,
-    robotEndMood: isCustom ? "happy" : puzzles[puzzleID].robotEndMood,
+    startingText: isCustom
+      ? customStartingText
+      : puzzles[puzzleID].startingText,
+    hintText: isCustom ? customHintText : puzzles[puzzleID].hintText,
+    winText: isCustom ? customWinText : puzzles[puzzleID].winText,
+    message: isCustom ? customStartingText : puzzles[puzzleID].startingText,
+    robotStartMood: isCustom
+      ? customRobotMood
+      : puzzles[puzzleID].robotStartMood,
+    robotEndMood: isCustom ? customRobotMood : puzzles[puzzleID].robotEndMood,
     puzzle,
     numColumns,
     numRows,
@@ -105,8 +140,6 @@ export function gameInit({
     maxNumber,
     validNextIndexes,
     mainPath,
-    encodedPuzzle,
     mouseIsActive: false,
-    puzzleID: isCustom ? "custom" : puzzleID,
   };
 }
