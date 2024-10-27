6a5a72d2f5729641c0704891d96d9af03afc2f0e
reasonb4belief <colinthom89@gmail.com>
2024-10-27 03:03:03 +0000

    updated portal mania, added Chest, Enterprize, Eye and Needle sations

--- src/logic/forColinConversion.js
+++ src/logic/forColinConversion.js
@@ -53,10 +53,12 @@ export function getPuzzlesForColin({
 // 1. Fill in the text below and put the URLs in inputURLs as a comma separated list of strings
 // 2. In the terminal, run: npm run testrun src/logic/forColinConversion.js
 // 3. Copy the printed output (minus the open and closing square brackets)
+// 4. To test, run: npm t -- src/logic/puzzles.test.js
 
-const stationName = "Chest Station";
+const stationName = "Needle Station";
 
-const startingText = "Yo ho ho and a bottle of slime!";
+const startingText = "Even a camel could pass through this one!"
+;
 
 const winText =
   "Nice job! Let us know what you thought of the game at www.patreon.com/c/skedwards88. We’re still in beta, so all feedback is appreciated!";
@@ -66,15 +68,19 @@ const robotStartMood = "happy";
 const robotEndMood = "happy";
 
 const inputURLs = [
-  "https://skedwards88.github.io/deep-space-slime?id=Chest+1_OOOOOOOOOOOOOOOOOOOOOOPFFFPOOFFEFFOOFFSFFOOPFFFPOOOOOOOOOOOOOOO",
+"https://skedwards88.github.io/deep-space-slime?id=Needle+1_OOBEBOOOOBBBOOOJBBBJOOBFOFBOOBOOOBOOBOOOBOOBFOFBOOJBBBJOOOBSBOO",
 
-  "https://skedwards88.github.io/deep-space-slime?id=Chest+2_OOOOOOOOOOOOOOOOOOOOOOPF4FPOOFFEFFOO2FSF1OOPF3FPOOOOOOOOOOOOOOO",
+"https://skedwards88.github.io/deep-space-slime?id=Needle+2_OOBEBOOOOFDFOOOOPFKOOOOBFPOOOOKDBOOOOPFKOOOOBDPOOOOFBFOOOOBSBOO",
 
-  "https://skedwards88.github.io/deep-space-slime?id=Chest+3_OOOOOOOOOOOOOOOOOOOOOOP4F3POOFFEFFOO2FSF1OOPFFFPOOOOOOOOOOOOOOO",
+"https://skedwards88.github.io/deep-space-slime?id=Needle+3_OOBEBOOOOFDKOOOOPB2OOOOBFPOOOOKBFOOOOP1BOOOOBFPOOOOFDFOOOOBSBOO",
 
-  "https://skedwards88.github.io/deep-space-slime?id=Chest+4_OOOOOOOOOOOOOOOOOOOOOOPFFFPOOF4E5FOO2FSF3OOPF1FPOOOOOOOOOOOOOOO",
+"https://skedwards88.github.io/deep-space-slime?id=Needle+4_OOBEBOOOOBPBOOOO2B3OOOOBPBOOOOBFBOOOOBPBOOOO4B1OOOOBPBOOOOBSBOO",
 
-  "https://skedwards88.github.io/deep-space-slime?id=Chest+5_OOOOOOOOOOOOOOOOOOOOOOPF4FPOOF5E3FOO2FSF1OOPFFFPOOOOOOOOOOOOOOO",
+"https://skedwards88.github.io/deep-space-slime?id=Needle+5_OOBEBOOOOF2FOOOOPBBOOOO3BPOOOOBFBOOOOPBFOOOOB4POOOOFB1OOOOBSBOO",
+
+"https://skedwards88.github.io/deep-space-slime?id=Needle+6_OOBEBOOOOFBKOOOOPD1OOOOBBPOOOOK3JOOOOPBBOOOO2DPOOOOFBBOOOOBSBOO",
+
+"https://skedwards88.github.io/deep-space-slime?id=Needle+7_OOOEOOOOOOFOOOOOOPOOOOOPPPOOOO5JBOOOOB1BOOOO3B2OOOO4BBOOOOPSPOO",
 ];
 
 console.log(
