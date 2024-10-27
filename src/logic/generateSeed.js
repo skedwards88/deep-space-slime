4618f9ce01dabdbf47d6482c22d99abd758e7a5b
Sarah <skedwards88@gmail.com>
2024-10-27 10:33:25 -0700

    standardize custom- start of share link

--- src/logic/generateSeed.js
+++ src/logic/generateSeed.js
@@ -2,5 +2,5 @@ export function generateSeed(roomName, encodedPuzzle) {
   // Convert any spaces in the name to + to be able to pass in a query parameter
   const sanitizedName = roomName.replaceAll(" ", "+");
 
-  return `${sanitizedName}_${encodedPuzzle}`;
+  return `custom-${sanitizedName}_${encodedPuzzle}`;
 }
