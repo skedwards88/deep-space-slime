0d810aec59beb4dbe1e8ea74817bcc8bbbcb5e7d
Sarah <skedwards88@gmail.com>
2024-11-14 04:17:04 -0800

    partial implementation of sharing to get hints. only applies to one share button right now

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {handleShare} from "../common/handleShare";
 
-export default function Share({appName, text, url}) {
+export default function Share({appName, text, url, seed, secondaryEffect}) {
   if (navigator.canShare) {
     return (
-      <button onClick={() => handleShare({appName, text, url})}>Share</button>
+      <button
+        onClick={() => {
+          if (secondaryEffect) {
+            secondaryEffect();
+          }
+          handleShare({appName, text, url, seed});
+        }}
+      >
+        Share
+      </button>
     );
   } else {
     return <></>;
