e0b9778d133fbd390ebe7166817548f05a762074
Sarah <skedwards88@gmail.com>
2024-11-29 09:49:10 -0800

    split custom and non custom init logic

--- src/logic/gameInit.js
+++ src/logic/gameInit.js
@@ -6,13 +6,7 @@ import {validateCustomPuzzle} from "./validateCustomPuzzle";
 import {convertStringToPuzzle} from "./convertPuzzleString";
 import {features, numColumns, numRows} from "./constants";
 
-export function gameInit({
-  useSaved = true,
-  puzzleID = 0,
-  isCustom = false,
-  customSeed,
-  customIndex,
-}) {
+function customInit({useSaved, customSeed, customIndex}) {
   const customStationName = "Custom Simulation"; // todo could set elsewhere for import
   const customWinText =
     "You solved the custom puzzle! You can edit or share the custom puzzle, or return to the main game.";
@@ -21,6 +15,77 @@ export function gameInit({
   const customHintText = undefined;
   const customRobotMood = "happy";
 
+  // Return the saved state if we can
+  let savedState = useSaved
+    ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))
+    : undefined;
+
+  if (savedState && validateSavedState(savedState)) {
+    return {
+      ...savedState,
+      mouseIsActive: false,
+      // Overwrite these properties in case we changed them mid-play.
+      // They don't affect the puzzle, so we don't need to reset the player's progress.
+      station: customStationName,
+      startingText: customStartingText,
+      hintText: customHintText,
+      winText: customWinText,
+      robotStartMood: customRobotMood,
+      robotEndMood: customRobotMood,
+    };
+  }
+
+  // Convert the query string into a puzzle
+  let customName;
+  let customEncodedPuzzle;
+  let puzzle;
+  try {
+    if (!customSeed.startsWith("custom-")) {
+      throw new Error("Custom seed did not start with 'custom-'");
+    }
+    customSeed = customSeed.substring("custom-".length);
+    [customName, customEncodedPuzzle] = customSeed.split("_");
+    customName = customName.replaceAll("+", " ");
+    puzzle = convertStringToPuzzle(customEncodedPuzzle);
+
+    // Make sure that the puzzle passes all of the validation (in case someone edits/mangles the query string)
+    const {isValid} = validateCustomPuzzle({
+      puzzle,
+      numColumns,
+      numRows,
+    });
+    if (!isValid) {
+      throw new Error("Custom puzzle is not valid.");
+    }
+  } catch (error) {
+    console.error("Error generating custom puzzle from query: " + error);
+    // If couldn't generate a puzzle, use the non-custom init instead
+    if (!useSaved) {
+      savedState = JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"));
+    }
+    let puzzleID = 0;
+    if (savedState.puzzleID && puzzleIdIsValid(savedState.puzzleID)) {
+      puzzleID = savedState.puzzleID;
+    }
+    return nonCustomInit({useSaved, puzzleID});
+  }
+
+  return {
+    isCustom: true,
+    customIndex,
+    puzzleID: "custom",
+    station: customStationName,
+    roomName: customName,
+    startingText: customStartingText,
+    hintText: customHintText,
+    winText: customWinText,
+    robotStartMood: customRobotMood,
+    robotEndMood: customRobotMood,
+    puzzle,
+  };
+}
+
+function nonCustomInit({useSaved, puzzleID}) {
   // Return the saved state if we can
   const savedState = useSaved
     ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))
@@ -32,71 +97,61 @@ export function gameInit({
       mouseIsActive: false,
       // Overwrite these properties in case we changed them mid-play.
       // They don't affect the puzzle, so we don't need to reset the player's progress.
-      station: savedState.isCustom
-        ? customStationName
-        : puzzles[savedState.puzzleID].station,
-      roomName: savedState.isCustom
-        ? savedState.roomName
-        : puzzles[savedState.puzzleID].roomName,
-      startingText: savedState.isCustom
-        ? customStartingText
-        : puzzles[savedState.puzzleID].startingText,
-      hintText: savedState.isCustom
-        ? customHintText
-        : puzzles[savedState.puzzleID].hintText,
-      winText: savedState.isCustom
-        ? customWinText
-        : puzzles[savedState.puzzleID].winText,
-      robotStartMood: savedState.isCustom
-        ? customRobotMood
-        : puzzles[savedState.puzzleID].robotStartMood,
-      robotEndMood: savedState.isCustom
-        ? customRobotMood
-        : puzzles[savedState.puzzleID].robotEndMood,
+      station: puzzles[savedState.puzzleID].station,
+      roomName: puzzles[savedState.puzzleID].roomName,
+      startingText: puzzles[savedState.puzzleID].startingText,
+      hintText: puzzles[savedState.puzzleID].hintText,
+      winText: puzzles[savedState.puzzleID].winText,
+      robotStartMood: puzzles[savedState.puzzleID].robotStartMood,
+      robotEndMood: puzzles[savedState.puzzleID].robotEndMood,
     };
   }
 
   // If the saved state wasn't valid but we were instructed to use the saved state,
   // use the puzzleID from the saved state if possible
-  if (
-    useSaved &&
-    savedState &&
-    !savedState.isCustom &&
-    puzzleIdIsValid(savedState.puzzleID)
-  ) {
+  if (useSaved && savedState && puzzleIdIsValid(savedState.puzzleID)) {
     puzzleID = savedState.puzzleID;
   }
 
-  // If custom, convert the query string into a puzzle
-  let customName;
-  let customEncodedPuzzle;
-  let customPuzzle;
-  if (isCustom) {
-    try {
-      if (!customSeed.startsWith("custom-")) {
-        throw new Error("Custom seed did not start with 'custom-'");
-      }
-      customSeed = customSeed.substring("custom-".length);
-      [customName, customEncodedPuzzle] = customSeed.split("_");
-      customName = customName.replaceAll("+", " ");
-      customPuzzle = convertStringToPuzzle(customEncodedPuzzle);
+  const puzzle = puzzles[puzzleID].puzzle;
 
-      // Mane sure that the puzzle passes all of the validation (in case someone edits/mangles the query string)
-      const {isValid} = validateCustomPuzzle({
-        puzzle: customPuzzle,
-        numColumns,
-        numRows,
-      });
-      if (!isValid) {
-        throw new Error("Custom puzzle is not valid.");
-      }
-    } catch (error) {
-      console.error("Error generating custom puzzle from query: " + error);
-      isCustom = false;
-    }
+  return {
+    isCustom: false,
+    customIndex: undefined,
+    puzzleID,
+    station: puzzles[puzzleID].station,
+    roomName: puzzles[puzzleID].roomName,
+    startingText: puzzles[puzzleID].startingText,
+    hintText: puzzles[puzzleID].hintText,
+    winText: puzzles[puzzleID].winText,
+    robotStartMood: puzzles[puzzleID].robotStartMood,
+    robotEndMood: puzzles[puzzleID].robotEndMood,
+    puzzle,
+  };
+}
+
+export function gameInit({
+  useSaved = true,
+  puzzleID = 0,
+  isCustom = false,
+  customSeed,
+  customIndex,
+}) {
+  const savedCustom = useSaved
+    ? JSON.parse(localStorage.getItem("deepSpaceSlimeSavedState"))?.isCustom
+    : false;
+
+  const baseState =
+    isCustom || savedCustom
+      ? customInit({useSaved, customSeed, customIndex})
+      : nonCustomInit({useSaved, puzzleID});
+
+  // Use this as a proxy to see if using the saved state and can return here
+  if ("flaskCount" in baseState) {
+    return baseState;
   }
 
-  const puzzle = isCustom ? customPuzzle : puzzles[puzzleID].puzzle;
+  const puzzle = baseState.puzzle;
 
   const startIndex = puzzle.indexOf(features.start);
   const mainPath = [startIndex];
@@ -112,24 +167,12 @@ export function gameInit({
     maxNumber,
   });
 
-  sendAnalytics("new_game", {puzzleID: isCustom ? "custom" : puzzleID});
+  sendAnalytics("new_game", {
+    puzzleID: baseState.puzzleID,
+  });
 
   return {
-    isCustom,
-    customIndex,
-    puzzleID: isCustom ? "custom" : puzzleID,
-    station: isCustom ? customStationName : puzzles[puzzleID].station,
-    roomName: isCustom ? customName : puzzles[puzzleID].roomName,
-    startingText: isCustom
-      ? customStartingText
-      : puzzles[puzzleID].startingText,
-    hintText: isCustom ? customHintText : puzzles[puzzleID].hintText,
-    winText: isCustom ? customWinText : puzzles[puzzleID].winText,
-    robotStartMood: isCustom
-      ? customRobotMood
-      : puzzles[puzzleID].robotStartMood,
-    robotEndMood: isCustom ? customRobotMood : puzzles[puzzleID].robotEndMood,
-    puzzle,
+    ...baseState,
     flaskCount: 0,
     keyCount: 0,
     jetCount: 0,
