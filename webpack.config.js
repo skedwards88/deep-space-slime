05a3b9520c9ec9898d32ee0f80817c2c2e05a792
Sarah <skedwards88@gmail.com>
2024-12-04 07:44:12 -0800

    transfer music changes

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = (env, argv) => {
           use: ["style-loader", "css-loader"],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+          test: /\.(png|svg|jpg|jpeg|gif|webp|mp3)$/i,
           type: "asset/resource",
         },
       ],
