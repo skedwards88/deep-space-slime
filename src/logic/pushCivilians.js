14d42235fc574cbc1cdf981a18472dbcb36516ab
skedwards88 <skedwards88@gmail.com>
2024-12-11 13:24:39 -0800

    add pushCivilians

---
+++ src/logic/pushCivilians.js
@@ -0,0 +1,32 @@
+import {getNextAdjacentIndex} from "./getNextAdjacentIndex";
+import {numColumns, numRows} from "./constants";
+
+export function pushCivilians({pushedFrom, pushedCivilian, civilians}) {
+  let newCivilians = [...civilians];
+
+  while (pushedCivilian !== undefined) {
+    const civilianEndIndex = getNextAdjacentIndex({
+      index: pushedFrom,
+      adjacentIndex: pushedCivilian,
+      numColumns,
+      numRows,
+    });
+
+    if (civilianEndIndex === undefined) {
+      return newCivilians;
+    }
+
+    const civilianPositionToUpdate = civilians.indexOf(pushedCivilian);
+    newCivilians[civilianPositionToUpdate] = civilianEndIndex;
+
+    const civilianDomino = civilians.includes(civilianEndIndex);
+    if (civilianDomino) {
+      pushedFrom = pushedCivilian;
+      pushedCivilian = civilianEndIndex;
+    } else {
+      pushedCivilian = undefined;
+    }
+  }
+
+  return newCivilians;
+}
---
+++ src/logic/pushCivilians.test.js
@@ -0,0 +1,65 @@
+import {pushCivilians} from "./pushCivilians";
+
+describe("pushCivilians", () => {
+  test("push one civilian", () => {
+    const result = pushCivilians({
+      pushedFrom: 0,
+      pushedCivilian: 1,
+      civilians: [1],
+    });
+
+    expect(result).toEqual(expect.arrayContaining([2]));
+  });
+
+  test("push a chain of civilians", () => {
+    const result = pushCivilians({
+      pushedFrom: 0,
+      pushedCivilian: 1,
+      civilians: [1, 2],
+    });
+
+    expect(result).toEqual(expect.arrayContaining([2, 3]));
+  });
+
+  test("unpushed civilians are not affected", () => {
+    const result = pushCivilians({
+      pushedFrom: 0,
+      pushedCivilian: 1,
+      civilians: [1, 2, 4],
+    });
+
+    expect(result).toEqual(expect.arrayContaining([2, 3, 4]));
+  });
+});
+
+describe("pushCivilians: pushing will not wrap", () => {
+  test("push to next row", () => {
+    const result = pushCivilians({
+      pushedFrom: 5,
+      pushedCivilian: 6,
+      civilians: [6],
+    });
+
+    expect(result).toEqual(expect.arrayContaining([6]));
+  });
+
+  test("CAUTION: Expects civilian push validation to happen before calling; otherwise, can double up civilians", () => {
+    const result = pushCivilians({
+      pushedFrom: 4,
+      pushedCivilian: 5,
+      civilians: [5, 6],
+    });
+
+    expect(result).toEqual(expect.arrayContaining([6, 6]));
+  });
+
+  test("chained push across row", () => {
+    const result = pushCivilians({
+      pushedFrom: 5,
+      pushedCivilian: 6,
+      civilians: [6, 7, 8],
+    });
+
+    expect(result).toEqual(expect.arrayContaining([6, 7, 8]));
+  });
+});
